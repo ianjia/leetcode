fe674b87b56be8e216be80cde948556554a464b9
ianjia <ianjiawa@gmail.com>
2020-06-24 17:29:10 -0700

    Added Two Sum solutions

---
+++ LeetCode1_TwoSum.py
@@ -0,0 +1,29 @@
+# class Solution(object):
+#     def twoSum(self, nums, target):
+#         """
+#         :type nums: List[int]
+#         :type target: int
+#         :rtype: List[int]
+#         """
+#         for i in range(0, len(nums)):
+#             for j in range(i + 1, len(nums)):
+#                 if nums[i] + nums[j] == target:
+#                     return [i, j]
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dictionary = {}
+        pos = 0
+        while pos < len(nums):
+            if (target - nums[pos]) not in dictionary:
+            #checks if the counterpart of nums[pos] is in dictionary
+            #for example, when 18/target - 7/nums[pos], the counterpart of nums[pos]/7 is 11 because 18 - 7 = 11
+                dictionary[nums[pos]] = pos
+                pos += 1
+            else:
+                return [dictionary[target - nums[pos]], pos]
