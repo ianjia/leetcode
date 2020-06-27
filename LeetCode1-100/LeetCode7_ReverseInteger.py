043d826a957459e0610e6069445904b7e4fe3c48
ianjia <ianjiawa@gmail.com>
2020-06-27 11:14:59 -0700

    Added reverse integer (problem 7)'

---
+++ LeetCode1-100/LeetCode7_ReverseInteger.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        string_of_x = str(abs(x))
+            
+        rev = int(string_of_x[::-1])
+        
+        if rev > 2147483648:
+            return 0
+        
+        if x > 0:
+            return rev
+        else:
+            rev = rev * -1
+            return rev
