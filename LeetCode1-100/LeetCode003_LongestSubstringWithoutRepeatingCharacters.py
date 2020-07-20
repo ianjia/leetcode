8b4e45b4ba16e41dba49ae44c9818d7c52bfb832
ianjia <ianjiawa@gmail.com>
2020-07-20 16:57:37 -0700

    Edited problem 3

--- LeetCode1-100/LeetCode003_LongestSubstringWithoutRepeatingCharacters.py
+++ LeetCode1-100/LeetCode003_LongestSubstringWithoutRepeatingCharacters.py
@@ -1,19 +1,20 @@
 # Sliding Window (two-pointers)
-class Solution(object):
+class Solution1(object):
     def lengthOfLongestSubstring(self, s):
         """
         :type s: str
         :rtype: int
         """
-        visited = set() # use set because speed is O(log n) (list speed is O(n))
+        visited = set()
         slow, fast, curMax = 0, 0, 0
         
         while slow < len(s) and fast < len(s):
             if s[fast] not in visited:
                 visited.add(s[fast])
                 fast += 1
-                curMax = max(curMax, fast - slow)
+                curMax = max(curMax, fast - slow) # fast - slow (distance between fast and slow)
             else:
                 visited.remove(s[slow])
                 slow += 1
+                
         return curMax
