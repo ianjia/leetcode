7d04f2c929d5311a44f65584079b8bd1c2d0f41c
ianjia <ianjiawa@gmail.com>
2020-07-20 17:58:13 -0700

    Edited problem 12

--- LeetCode1-100/LeetCode012_IntegerToRoman.py
+++ LeetCode1-100/LeetCode012_IntegerToRoman.py
@@ -48,3 +48,22 @@ class Solution2(object):
         tens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
         ones = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
         return thousands[num // 1000] + hundreds[num % 1000 // 100] + tens[num % 100 // 10] + ones[num % 10]
+
+class Solution3(object):
+    def intToRoman(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        digits = [(1000, "M"), (900, "CM"), (500, "D"), (400, "CD"), (100, "C"), (90, "XC"), 
+          (50, "L"), (40, "XL"), (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I")]
+        
+        romanDigits = []
+        # Loop through each symbol.
+        for value, symbol in digits:
+            # We don't want to continue looping if we're done
+            if num == 0: break
+            count, num = divmod(num, value)
+            # Append "count" copies of "symbol" to romanDigits
+            romanDigits.append(symbol * count)
+        return "".join(romanDigits)
