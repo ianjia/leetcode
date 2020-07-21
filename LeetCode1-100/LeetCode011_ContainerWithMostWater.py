c035f10d803b96c1e5672fb7cdad45ba3303ab76
ianjia <ianjiawa@gmail.com>
2020-07-20 17:22:13 -0700

    Edited problem 11

--- LeetCode1-100/LeetCode011_ContainerWithMostWater.py
+++ LeetCode1-100/LeetCode011_ContainerWithMostWater.py
@@ -1,5 +1,5 @@
 # Two-pointers
-class Solution(object):
+class Solution1(object):
     def maxArea(self, height):
         """
         :type height: List[int]
@@ -9,8 +9,8 @@ class Solution(object):
         
         while left < right:
             # If we try to move the pointer at the longer line inwards, we won't be able to add any extra area because it is limited by the shorter line.
-            # In other words, even if the longer line is longer than the shorter line, you only can calculate the area of the shorter line.
-            maxWater = max(maxWater, min(height[left], height[right]) * (right - left))
+            # In other words, even if the longer line is longer than the shorter line, you only can calculate the area according to the shorter line.
+            maxWater = max(maxWater, min(height[left], height[right]) * (right - left)) # min part (find shorter line then multiply it by the length from left to right to get area)
             if height[left] < height[right]:
                 left += 1
             else:
