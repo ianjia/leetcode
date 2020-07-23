d0c70fc214c9f33de12d6dc82d313527a7daa92c
ianjia <ianjiawa@gmail.com>
2020-07-22 18:08:06 -0700

    Added problem 38

---
+++ LeetCode1-100/LeetCode038_CountAndSay.py
@@ -0,0 +1,31 @@
+# String
+class Solution(object):
+    def countAndSay(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        if n < 1:
+            return ""
+        
+        if n == 1:
+            return "1"
+        
+        res = "1"
+        
+        while n > 1:
+            count = 1
+            curStr = ""
+            for i in range(1, len(res)):
+                if res[i] == res[i-1]:
+                    count += 1
+                else:
+                    curStr += str(count)
+                    curStr += res[i-1]
+                    count = 1 # reset count   
+            curStr += str(count)
+            curStr += res[-1]
+            res = curStr
+            n -= 1
+        
+        return res
