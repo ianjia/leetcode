cd5026a0c826729e79c5fd3f8e06605e30ad5ae6
ianjia <ianjiawa@gmail.com>
2020-07-25 11:25:30 -0700

    Added problem 206

---
+++ LeetCode201-300/LeetCode206_ReverseLinkedList.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+# Iterative
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        newHead = None
+        
+        while head != None:
+            tempNode = head.next
+            head.next = newHead
+            newHead = head
+            head = tempNode
+            
+        return newHead
