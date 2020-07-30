8035467cd90a95291e8739778e271a98198d96c8
ianjia <ianjiawa@gmail.com>
2020-07-29 22:18:39 -0700

    Added solution for problem 5

--- LeetCode1-100/LeetCode005_LongestPalindromicSubstring.py
+++ LeetCode1-100/LeetCode005_LongestPalindromicSubstring.py
@@ -27,3 +27,40 @@ class Solution1(object):
             right += 1
             
         return s[left + 1:right]
+
+# Dynamic Programming
+class Solution2(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        n = len(s)
+        
+        if n == 0:
+            return ""
+        
+        f = [[False for col in range(n)] for row in range(n)]
+        left, length = 0, 1
+        
+        # Set True for every lenth 1 character
+        for i in range(0, n):
+            f[i][i] = True
+            
+        # set True for every length 2 characters if they equal
+        for i in range(0, n - 1):
+            if s[i] == s[i+1]:
+                f[i][i+1] = True
+                length = 2
+                left = i
+                
+        # Set True for every length 3 to N characters if they are palindromes
+        for curLen in range(3, n + 1):
+            for i in range(0, n - curLen + 1):
+                j = i + curLen - 1
+                f[i][j] = (s[i] == s[j]) and f[i+1][j-1]
+                if f[i][j] == True:
+                    length = curLen
+                    left = i
+
+        return s[left:left + length] # return substring of length "length"
